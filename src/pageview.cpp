1f2823a7d9db04ddef0df11338989c1c5d4206a3
paniszcze <paniszcze@gmail.com>
2024-06-07 21:20:15 +0200

    use pinch gesture for zooming in and out

--- src/pageview.cpp
+++ src/pageview.cpp
@@ -3,28 +3,84 @@
 PageView::PageView(QWidget *parent)
     : QGraphicsView(parent)
 {
-    mReader = new Reader();
-    mPage = new Page();
-    setScene(mPage);
+    // create OCR instance
+    reader = new Reader();
 
+    // create and set graphics scene
+    page = new Page();
+    setScene(page);
+
+    // enable pinch gesture
     grabGesture(Qt::PinchGesture);
     setAttribute(Qt::WA_AcceptTouchEvents);
 }
 
 PageView::~PageView()
 {
-    delete mReader;
+    delete reader;
 }
 
 void PageView::loadImg(QString filename, QStandardItemModel *translations)
 {
     QPixmap img = QPixmap(filename);
-    QGraphicsPixmapItem *imgItem = mPage->addPixmap(img);
+    QGraphicsPixmapItem *imgItem = page->addPixmap(img);
     fitInView(imgItem, Qt::KeepAspectRatio);
 
-    QList<QRect> resultRecs = mReader->readImg(filename, translations);
+    QList<QRect> resultRecs = reader->readImg(filename, translations);
 
     for (auto rect : resultRecs) {
-        mPage->addItem(new TranslationRect{rect});
+        page->addItem(new TranslationRect{rect});
     }
+}
+
+bool PageView::event(QEvent *event)
+{
+    if (event->type() == QEvent::Gesture) {
+        return gestureEvent(static_cast<QGestureEvent *>(event));
+    }
+    return QGraphicsView::event(event);
+}
+
+bool PageView::gestureEvent(QGestureEvent *event)
+{
+    if (QGesture *pinch = event->gesture(Qt::PinchGesture))
+        pinchTriggered(static_cast<QPinchGesture *>(pinch));
+    return true;
+}
+
+void PageView::resizeEvent(QResizeEvent *event)
+{
+    return QGraphicsView::resizeEvent(event);
+}
+
+void PageView::handleGestureEvent(QGestureEvent *gesture)
+{
+    if (QGesture *pinch = gesture->gesture(Qt::PinchGesture))
+        pinchTriggered(static_cast<QPinchGesture *>(pinch));
+}
+
+void PageView::pinchTriggered(QPinchGesture *gesture)
+{
+    if (gesture->changeFlags() & QPinchGesture::ScaleFactorChanged) {
+        setScaleValue(gesture->scaleFactor());
+    }
+}
+
+void PageView::setScaleValue(qreal factor)
+{
+    scaleValue *= factor;
+    scale(factor, factor);
+
+    // keep the zoom inbetween MIN and MAX
+    if (factor < 1 && scaleValue < MIN_SCALE) {
+        const qreal minv = MIN_SCALE / scaleValue;
+        scale(minv, minv);
+        scaleValue *= minv;
+    } else if (factor > 1 && scaleValue > MAX_SCALE) {
+        const qreal maxv = MAX_SCALE / scaleValue;
+        scale(maxv, maxv);
+        scaleValue *= maxv;
+    }
+
+    emit scaled(scaleValue * 100);
 }
--- src/pageview.h
+++ src/pageview.h
@@ -1,18 +1,21 @@
 #ifndef PAGEVIEW_H
 #define PAGEVIEW_H
 
+#include <QEvent>
+#include <QGestureEvent>
 #include <QGraphicsPixmapItem>
 #include <QGraphicsScene>
 #include <QGraphicsView>
-#include <QHBoxLayout>
 #include <QObject>
 #include <QPen>
 #include <QPixmap>
-#include <QWidget>
 
 #include "page.h"
 #include "reader.h"
 #include "translationrect.h"
+
+const qreal MAX_SCALE = 20.0;
+const qreal MIN_SCALE = 0.03;
 
 class PageView : public QGraphicsView
 {
@@ -22,10 +25,27 @@ public:
     PageView(QWidget *parent = nullptr);
     ~PageView();
 
-    Page *mPage;
-    Reader *mReader;
-
     void loadImg(QString filename, QStandardItemModel *translations);
+    bool event(QEvent *event) override;
+    bool gestureEvent(QGestureEvent *event);
+    void setScaleValue(qreal factor);
+
+public slots:
+    void resizeEvent(QResizeEvent *event) override;
+
+protected:
+    void handleGestureEvent(QGestureEvent *gesture);
+    void pinchTriggered(QPinchGesture *gesture);
+
+signals:
+    void scaled(qreal percent);
+
+public:
+    Page *page;
+    Reader *reader;
+
+private:
+    qreal scaleValue = 1.0;
 };
 
 #endif // PAGEVIEW_H
