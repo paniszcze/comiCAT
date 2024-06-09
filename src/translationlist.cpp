1da7405c3ba12db85e005cfc4a70e1afc92b9998
paniszcze <paniszcze@gmail.com>
2024-06-10 01:28:20 +0200

    change table view style to override dark theme

--- src/translationlist.cpp
+++ src/translationlist.cpp
@@ -6,16 +6,21 @@ TranslationList::TranslationList(QWidget *parent,
 {
     setObjectName("TranslationList");
     setStyleSheet(
-        "QWidget#TranslationList {border-radius: 4; border: 1px solid #e0e0e0;}"
+        "QWidget#TranslationList"
+        "{border-radius: 4; border: 1px solid #e0e0e0;}"
+        "QLabel#Translations"
+        "{padding-left: 1px; font-size: 13px; color: black;}"
+        "QLabel#StatusLabel {color: #5c5c5c;font-size: 11px;}"
+        "QComboBox {font-size: 12px; color: #3c3c3c;}"
+        "QTableView {border: 0; background-color: white;"
+        "font-weight: 300; font-size: 11px;}"
+        "QTableView::item {border-bottom: 1px solid #e0e0e0;"
+        "padding: 6px 0; color: black;}"
+        "QTableView::item:selected {background-color: #f2f2f2;}"
         "QHeaderView {background-color: white; color:#5c5c5c;}"
         "QHeaderView::section {background-color: white;"
         "border: 0; border-bottom: 1px solid #e0e0e0; padding-bottom: 4px;}"
-        "QComboBox {font-size: 12px; color: #3c3c3c;}"
-        "QScrollBar::handle {background-color: white;}"
-        "QLabel#Translations {padding-left: 1px;}"
-        "QLabel#StatusLabel {color: #5c5c5c;font-size: 11px;}"
-        "QTableView {border: 0; background-color: white; font-size: 11px;}"
-        "QTableView::item {border-bottom: 1px solid #e0e0e0; padding: 4px 0;}");
+        "QScrollBar::handle {background-color: white;}");
 
     QLabel *title = new QLabel("Translations");
     title->setObjectName("Translations");
