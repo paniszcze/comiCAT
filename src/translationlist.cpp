13e85e519569c428ec1c9f84422f8d717ac307b4
paniszcze <paniszcze@gmail.com>
2024-06-26 22:15:09 +0200

    change header view style

--- src/translationlist.cpp
+++ src/translationlist.cpp
@@ -6,18 +6,18 @@ TranslationList::TranslationList(QWidget *parent,
 {
     setObjectName("TranslationList");
     setStyleSheet(
-        "QWidget#TranslationList"
-        "{border-radius: 4; border: 1px solid #e0e0e0;}"
+        "QWidget#TranslationList {border-radius: 4; border: 1px solid #e0e0e0;}"
         "QLabel#Translations"
-        "{padding-left: 1px; font-size: 13px; color: black;}"
-        "QLabel#StatusLabel {color: #5c5c5c;font-size: 11px;}"
-        "QComboBox {font-size: 12px; color: #3c3c3c;}"
+        "{padding-left: 1px; color: black; font-size: 13px;}"
+        "QLabel#StatusLabel {color: #5c5c5c; font-size: 11px;}"
+        "QComboBox {color: #3c3c3c; font-size: 12px;}"
         "QTableView {border: 0; background-color: white;"
         "font-weight: 300; font-size: 11px;}"
         "QTableView::item {border-bottom: 1px solid #e0e0e0;"
         "padding: 6px 0; color: black;}"
         "QTableView::item:selected {background-color: #f2f2f2;}"
-        "QHeaderView {background-color: white; color:#5c5c5c;}"
+        "QHeaderView {background-color: white; color:#5c5c5c;"
+        "font-weight: 300;}"
         "QHeaderView::section {background-color: white;"
         "border: 0; border-bottom: 1px solid #e0e0e0; padding-bottom: 4px;}"
         "QScrollBar::handle {background-color: white;}");
