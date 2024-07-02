9a1c3cdb3da0655f6e1fe85324efba28de8eaadc
paniszcze <paniszcze@gmail.com>
2024-07-02 13:30:45 +0200

    draw border outside rect, fix bounding rect

--- src/translationrect.cpp
+++ src/translationrect.cpp
@@ -9,6 +9,13 @@ TranslationRect::TranslationRect(const QRect &rect, QGraphicsItem *parent)
     setFlags(QGraphicsItem::ItemIsSelectable);
 }
 
+QRectF TranslationRect::boundingRect() const
+{
+    QRectF boundingRect = QGraphicsRectItem::boundingRect();
+    boundingRect.adjust(-PEN_WIDTH, -PEN_WIDTH, PEN_WIDTH, PEN_WIDTH);
+    return boundingRect;
+}
+
 void TranslationRect::paint(QPainter *painter,
                             const QStyleOptionGraphicsItem *option,
                             QWidget *widget)
@@ -16,6 +23,9 @@ void TranslationRect::paint(QPainter *painter,
     Q_UNUSED(option);
     Q_UNUSED(widget);
 
-    painter->setPen({isSelected() ? Qt::green : Qt::magenta, 3});
-    painter->drawRect(boundingRect());
+    painter->setPen({isSelected() ? Qt::green : Qt::magenta, PEN_WIDTH});
+    painter->drawRect(QRectF(rect().x() - PEN_WIDTH / 2,
+                             rect().y() - PEN_WIDTH / 2,
+                             rect().width() + PEN_WIDTH,
+                             rect().height() + PEN_WIDTH));
 }
--- src/translationrect.h
+++ src/translationrect.h
@@ -1,20 +1,27 @@
 #ifndef TRANSLATIONRECT_H
 #define TRANSLATIONRECT_H
 
+#include <QGraphicsItem>
 #include <QGraphicsRectItem>
 #include <QPainter>
 #include <QPen>
+#include <QRect>
+#include <QRectF>
 #include <QStyle>
 #include <QStyleOptionGraphicsItem>
+#include <QWidget>
+
+static const qreal PEN_WIDTH = 2;
 
 class TranslationRect : public QGraphicsRectItem
 {
 public:
     TranslationRect(const QRect &rect, QGraphicsItem *parent = nullptr);
 
+    QRectF boundingRect() const override;
     void paint(QPainter *painter,
                const QStyleOptionGraphicsItem *option,
-               QWidget *widget);
+               QWidget *widget) override;
 };
 
 #endif // TRANSLATIONRECT_H
